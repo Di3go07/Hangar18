3ab7ce75aad9fed03b8219eca89386e10dcac95c
Di3go <diego.dpab@gmail.com>
2026-01-08 15:53:35 -0300

    Commit - configurações na aplicação e criação dos primeiros models para o banco de dados

--- hangar18/models.py
+++ hangar18/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class Users(models.Model):
+    class Cargo(models.TextChoices):
+        AUTOR = 'autor'
+        EDITOR = 'editor'
+        ADMINISTRADOR = 'administrador'
+
+    nome: models.CharField(max_length=100)
+    email: models.EmailField()
+    password: models.CharField(max_length=128)
+    cargo: models.CharField(
+        choices = Cargo.choices,
+        default = Cargo.AUTOR
+    )
+
+class Publications(models.Model):
+    class Types(models.TextChoices):
+        NOTICIA = "noticia"
+        MATERIA = "matéria"
+        RESENHA = "resenha"
+
+    title: models.CharField(max_length=100)
+    subtitle: models.CharField(max_length=250)
+    author = models.ForeignKey(Users, on_delete=models.PROTECT)
+    artcileType = models.CharField(
+        choices = Types.choices,
+        default = Types.NOTICIA
+    )
+    slug: models.SlugField(max_length=250, unique=True, blank=True)
+    date: models.DateField()
+    edited: models.DateField(null=True, blank=True)
--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import hangar18.views as func
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users', func.users)
 ]
