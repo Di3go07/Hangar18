bad7b427527a1f43891d033e4075f9c742a19780
Di3go <diego.dpab@gmail.com>
2026-01-09 09:26:46 -0300

    Alterações no administrador da aplicação Django

--- hangar18/admin.py
+++ hangar18/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from hangar18.models import User, Publication
 
-# Register your models here.
+class userAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'password', 'cargo')
+    list_display_links = ('id', 'nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(User, userAdmin)
+
+class publicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'subtitle', 'author', 'artcileType', 'slug', 'date', 'edited')
+    list_display_links = ('id', 'title', 'artcileType', )
+    list_per_page = 20
+    search_fields = ('title', )
+
+admin.site.register(Publication, publicationAdmin)
+
--- hangar18/models.py
+++ hangar18/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 from django.forms import CharField
 
-class Users(models.Model):
+class User(models.Model):
     class Cargo(models.TextChoices):
         AUTOR = 'autor'
         EDITOR = 'editor'
         ADMINISTRADOR = 'administrador'
 
-    nome: models.CharField(max_length=100)
-    email: models.EmailField()
-    password: models.CharField(max_length=128)
-    cargo: models.CharField(
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    cargo = models.CharField(
         choices = Cargo.choices,
         default = Cargo.AUTOR
     )
 
-class Publications(models.Model):
+class Publication(models.Model):
     class Types(models.TextChoices):
         NOTICIA = "noticia"
         MATERIA = "matéria"
         RESENHA = "resenha"
 
-    title: models.CharField(max_length=100)
-    subtitle: models.CharField(max_length=250)
-    author = models.ForeignKey(Users, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
     artcileType = models.CharField(
         choices = Types.choices,
         default = Types.NOTICIA
     )
-    slug: models.SlugField(max_length=250, unique=True, blank=True)
-    date: models.DateField()
-    edited: models.DateField(null=True, blank=True)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
+    date = models.DateField()
+    edited = models.DateField(blank=True, null=True)
